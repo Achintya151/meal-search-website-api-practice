e1bd8376be5013f7b0112cf4c3ca93e32bd4d6a8
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-10 18:34:59 +0600

    meal list added

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+const getMealList = () => {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    // console.log(searchInputTxt);
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                    <div class = "meal-item" data-id ="${meal.idMeal}">
+                     <div class = "meal-img">
+                         <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                    <div class = "meal-name">
+                        <h3>${meal.strMeal}</h3>
+                        <a href = "#" class = "recipe-btn">Get Recipe</a>
+                     </div>
+                    </div>
+                    `;
+                });
+            }
+
+            mealList.innerHTML = html;
+        });
+}
+
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+
